44dc79e9a31188d1841d74313355f7cdb4eaba97
Suad Abrar <suadabrar44@gmail.com>
2025-11-25 15:15:52 +0300

    update loop folder

--- EMYC/TERM2/loop/script.js
+++ EMYC/TERM2/loop/script.js
@@ -60,29 +60,102 @@ let arrowFun = (a = 8, b = 9) => {
 arrowFun();
 
 //
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-array.map((array2) =>{
-    console.log(array2 * 2);
-})
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+array.map((array2) => {
+  console.log(array2 * 2);
+});
 
 //
 
-function operation(a,b){
-    function add(){
-        console.log("sum: " + (a + b));
-    }
-    add();
-    function sub(){
-        console.log("sub: " + (a - b));
-    }
-    sub();
-    function prod(){
-        console.log("prod: " + (a * b));
-    }
-    prod();
-    function div(){
-        console.log("div: " + (a / b));
-    }
-    div();
+function operation(a, b) {
+  function add() {
+    console.log("sum: " + (a + b));
+  }
+  add();
+  function sub() {
+    console.log("sub: " + (a - b));
+  }
+  sub();
+  function prod() {
+    console.log("prod: " + a * b);
+  }
+  prod();
+  function div() {
+    console.log("div: " + a / b);
+  }
+  div();
 }
-operation(4,2)
+operation(4, 2);
+
+// OBJECT
+
+// using object literal
+const person = {
+  name: "Abebe",
+  age: 29,
+  grade: 12,
+  greetPerson() {
+    console.log("Hello, " + this.name);
+  },
+  programmingSkill: {
+    back: "python",
+    frontend: "react",
+  },
+};
+
+// Using object constructor
+const student = new Object();
+student.name = "Ayele";
+student.age = "60";
+student.grade = 11;
+
+console.log(person.name);
+console.log(person["name"]);
+console.log(student);
+
+student.section = "A";
+student.age = 22;
+delete student.grade;
+console.log(student);
+person.greetPerson();
+console.log(person);
+console.log(person.programmingSkill.back);
+
+// object destructuring
+const { name, age } = person;
+console.log("name: ", name, "\n", "age: ", age);
+
+const dateTimeNow = new Date();
+console.log("Today: " + dateTimeNow);
+console.log(dateTimeNow.getFullYear());
+console.log(dateTimeNow.toDateString());
+
+const newDate = new Date("2025-11-23")
+console.log("Tomorrow: " + newDate);
+
+// 
+const myObject = {
+  fullName: "Suad Abrar",
+  age: 21,
+  gender: "F",
+  institution: "Mit",
+  course: "Full stack dev't"
+}
+
+function myFun(myObjectProperty){
+  const value = myObject[myObjectProperty]
+  console.log(value);
+  return;
+}
+
+myFun("fullName")
+
+const fruitNames = ["Mango", "Apple", "Orange", "Banana"];
+console.log(fruitNames);
+console.log(fruitNames.push("Avocado"));
+console.log(fruitNames.pop("Orange"));
+console.log(fruitNames);
+console.log(fruitNames.shift());
+// console.log(fruitNames);
+console.log(fruitNames.unshift("Orange"));
+// console.log(fruitNames);
